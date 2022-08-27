8eba0faf710eed47beb7622df8230a67a1c62ace
ngyenphing <75723993+ngyenphing@users.noreply.github.com>
2022-08-27 17:53:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
